23201ff429142cb588a118bd6089fd6a7b5ab3b6
Niclas Finne <niclas.finne@ri.se>
2021-05-28 14:00:21 +0200

    Added example to only use data from the network is stable

--- project/scripts/test-read-datatrace.py
+++ project/scripts/test-read-datatrace.py
@@ -12,9 +12,15 @@ def main():
     # Show summary
     # trace.print_summary()
 
+    network_events = trace.get_events(event_type='network', description='steady-state')
+    network_stable_time = network_events[0].time if network_events else 0
+
     data = []
     p = re.compile(r'.*DATA: (.+)$')
-    for o in trace.get_mote_output():
+    # Only look at mote output from after the network is stable
+    # Note that first statistics counters should not be counted as they might
+    # include data from before the network was stable.
+    for o in trace.get_mote_output(start_time=network_stable_time):
         m = p.match(o.message)
         if m:
             data.append([o.time, o.mote_id] + [int(v) for _k, v in (g.split(':') for g in m.group(1).split(','))])
